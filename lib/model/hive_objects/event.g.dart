2298b7ce70b9068992a300f58be673d42bca1cd3
yasin <yasinmiapolash@gmail.com>
2024-07-04 09:36:50 +0600

    update

---
+++ lib/model/hive_objects/event.g.dart
@@ -0,0 +1,55 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'event.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class EventAdapter extends TypeAdapter<Event> {
+  @override
+  final int typeId = 2;
+
+  @override
+  Event read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Event(
+      (fields[0] as HiveList).castHiveList(),
+      fields[1] as DateTime,
+      fields[2] as String,
+      fields[3] as String,
+      fields[5] as bool,
+    )..file = fields[4] as Uint8List?;
+  }
+
+  @override
+  void write(BinaryWriter writer, Event obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.category)
+      ..writeByte(1)
+      ..write(obj.date)
+      ..writeByte(2)
+      ..write(obj.eventName)
+      ..writeByte(3)
+      ..write(obj.eventDescription)
+      ..writeByte(4)
+      ..write(obj.file)
+      ..writeByte(5)
+      ..write(obj.completed);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is EventAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
