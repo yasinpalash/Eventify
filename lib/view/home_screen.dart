be5dc23abdd22a4bb42fcc26e912d1afcbdeda2a
yasin <yasinmiapolash@gmail.com>
2024-07-15 23:28:01 +0600

    tableCalender added

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -2,10 +2,18 @@ import 'package:calendar_app/utils/app_colors.dart';
 import 'package:calendar_app/utils/app_texts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime daySelected = DateTime.utc(
+      DateTime.now().year, DateTime.now().month, DateTime.now().day);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,9 +38,21 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: Column(children: [
-          
-        ],),
+        child: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2023, 1, 1),
+              lastDay: DateTime.utc(2030, 1, 1),
+              focusedDay: daySelected,
+              currentDay: daySelected,
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  daySelected = selectedDay;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
